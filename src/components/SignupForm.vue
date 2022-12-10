f9f8fd79ab7d2a8d58a7e40f12b5dfdfa6d25a9e
swiftly1111 <shinmaru518@gmail.com>
2022-12-10 19:13:03 +0900

    メッセージ表示機能まで作成2

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -6,6 +6,7 @@
       <input type="email" required placeholder="メールアドレス" v-model="email">
       <input type="password" required placeholder="パスワード" v-model="password">
       <input type="password" required placeholder="パスワード（確認用）" v-model="passwordConfirmation">
+      <div class="error">{{ error }}</div>
       <button>登録する</button>
     </form>
   </div>
@@ -13,17 +14,21 @@
 
 <script>
 import axios from 'axios'
+import setItem from '../auth/setItem'
 export default {
+  emits: ['redirectToChatRoom'],
   data () {
     return {
       name: '',
       email: '',
       password: '',
-      passwordConfirmation: ''
+      passwordConfirmation: '',
+      error: null
     }
   },
   methods: {
     async signUp () {
+      this.error = null
       try {
         const res = await axios.post('http://localhost:3000/auth', {
           name: this.name,
@@ -32,10 +37,18 @@ export default {
           password_confirmation: this.passwordConfirmation
           }
         )
+        if (!res) {
+          throw new Error('アカウントを登録できませんでした')
+        }
+        if (!this.error) {
+          setItem(res.headers, res.data.data.name)
+          this.$emit('redirectToChatRoom')
+        }
         console.log({ res })
         return res
       } catch (error) {
         console.log({ error })
+        this.error = 'アカウントを登録できませんでした'
       }
     }
   }
